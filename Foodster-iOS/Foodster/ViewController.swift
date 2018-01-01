60e67baceabdf155ac2636cb5ddeb2e0b35db32a
parushis <parushi@gmail.com>
2017-12-31 23:31:13 -0800

    Add files via upload
    Initial

---
+++ Foodster-iOS/Foodster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Foodster
+//
+//  Created by Parushi Sharma on 12/31/17.
+//  Copyright © 2017 Parushi Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
